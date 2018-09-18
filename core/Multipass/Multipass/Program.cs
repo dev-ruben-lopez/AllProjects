02aa1722172ff5ae71c24b455ae777afba41f549
dev-ruben-lopez <rubendariol@gmail.com>
2018-09-18 20:59:19 +0930

    Changes from MacBook

---
+++ core/Multipass/Multipass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Multipass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DoOne(10);
+            Console.Write("Resultado ?");
+            Console.Read();
+
+        }
+
+        
+        static async void DoOne(int param)
+        {
+            await Task.Delay(10000);
+            Console.Write("El resultado es : " + (param * 21).ToString()) ;
+        }
+    }
+}
