91a7f91ab01a06c351267f47b1a58a23dc8103fd
Ruben Lopez <dev.ruben.lopez@outlook.com>
2019-02-04 16:02:15 +1030

    Fixing ID server configuration;   Fixing Call from content directory from CitiesController to fake CitiesDataStore

--- core/CityInfo.API/CityInfo.API/Controller/CitiesController.cs
+++ core/CityInfo.API/CityInfo.API/Controller/CitiesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace CityInfo.API.Controller
 {
@@ -12,28 +13,34 @@ namespace CityInfo.API.Controller
     public class CitiesController : ControllerBase
     {
         private IHostingEnvironment _env;
+<<<<<<< HEAD
         private CitiesDataStore _citiesDataStore;
+=======
+
+        private CitiesDataStore cities;
+>>>>>>> Fixing ID server configuration;   Fixing Call from content directory from CitiesController to fake CitiesDataStore
 
         public CitiesController(IHostingEnvironment env)
         {
             _env = env;
+<<<<<<< HEAD
             _citiesDataStore = new CitiesDataStore(_env);
+=======
+            cities = new CitiesDataStore(_env);
+>>>>>>> Fixing ID server configuration;   Fixing Call from content directory from CitiesController to fake CitiesDataStore
         }
 
         [HttpGet()]
         public IActionResult GetCities()
         {
-            return Ok(new JsonResult(CitiesDataStore.Current.Cities));
+            return Ok(new JsonResult( cities.Cities));
         }
 
         [HttpGet("{id}")]
         public IActionResult GetCities(int id)
         {
-            var city = new JsonResult(CitiesDataStore.Current.Cities.Where(c => c.Id.Equals(id)).First());
-            if (city == null)
-                return NotFound();
-            else
-                return Ok(city);
+            var city = new JsonResult(cities.Cities.First(c => c.Id.Equals(id)));
+            return Ok(city);
         }
             
 
