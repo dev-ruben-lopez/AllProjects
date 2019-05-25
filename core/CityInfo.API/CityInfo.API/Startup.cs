1bd86ad7e36fa93ef156a921e9a600c9c64176fd
rlopezmachad <rlopezmachad@dxc.com>
2019-05-26 00:43:09 +0930

    Changes from dxc laptop 2

--- core/CityInfo.API/CityInfo.API/Startup.cs
+++ core/CityInfo.API/CityInfo.API/Startup.cs
@@ -7,48 +7,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Serialization;
-using SimpleInjector;
-using SimpleInjector.Lifestyles;
-using SimpleInjector.Integration.AspNetCore.Mvc;
 
 namespace CityInfo.API
 {
     public class Startup
     {
-
-        private Container container = new Container(); //Using container to resolve DI in this project.
-
-        private IHostingEnvironment _env;
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IHostingEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
-            _env = env;
-
-            services.AddMvc().
-                //THis is the way to manipulate or configure the JSON serializer settings in the responso
-                AddJsonOptions(o =>
-                {
-                    if (o.SerializerSettings.ContractResolver != null)
-                    {
-                        var castedResolver = o.SerializerSettings.ContractResolver
-                        as DefaultContractResolver;
-                        castedResolver.NamingStrategy = null; //THis will made that the JSON responses from the API are exactly as the model names and parameters
-
-                    }
-                });
-
-          
-            
-            // Default lifestyle scoped + async
-            // The recommendation is to use AsyncScopedLifestyle in for applications that solely consist of a Web API(or other asynchronous technologies such as ASP.NET Core)
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            //Add here Interfaces/Implementations in the container
-            
+            services.AddMvc();
 
         }
 
--- core/CityInfo.API/DataGenerator/Program.cs
+++ core/CityInfo.API/DataGenerator/Program.cs
@@ -1,4 +1,4 @@
-﻿using Bogus;//Data generator tool
+﻿using Bogus;
 using System;
 using CityInfo.API.Models;
 using Newtonsoft.Json;
@@ -14,7 +14,7 @@ namespace DataGenerator
 
             //Define the data rules for the generation
 
-            var testPointsOfInterest = new Faker<PointsOfInterestDto>()
+            var testPointsOfInterest = new Faker<PointsOfInterestForCreationDto>()
                 .RuleFor(p => p.Id, f => f.IndexFaker)
                 .RuleFor(p => p.Name, f => f.Lorem.Word())
                 .RuleFor(p => p.Description, f => f.Lorem.Paragraph(1));
